8834771c22a35624b5c9369df8d0324f094b2831
Gerardo Sandoval Romero <gerardo.sand.rom@gmail.com>
2025-05-30 22:38:54 -0600

    Setup Inicial Frontend

--- event-board/src/app/layout.tsx
+++ event-board/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import NavigationMenuWithDropdown from "@/components/customized/navigation-menu/navigation-menu-03";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,7 +28,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+        <NavigationMenuWithDropdown/>
         {children}
       </body>
     </html>
